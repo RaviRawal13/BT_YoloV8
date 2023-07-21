28534cca46217ba0832a3f4c4a47d288755ad1c5
Ravi Rawal <ravirawal13@gmail.com>
2023-07-21 23:01:06 +0530

    update

--- app.py
+++ app.py
@@ -68,7 +68,7 @@ with col2:
         else:
             if st.sidebar.button('Detect Objects'):
                 res = model.predict(uploaded_image,
-                                    conf=confidence
+                                    conf=confidence,device="cpu" 
                                     )
                 boxes = res[0].boxes
                 res_plotted = res[0].plot()[:, :, ::-1]
